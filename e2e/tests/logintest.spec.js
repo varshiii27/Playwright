51b2d780da9df5ca68da399a9cfb44d1a17c1e63
Varshitha <varshithaperam27@gmail.com>
2025-09-14 19:24:06 +0530

    Add logintest.spec.js to e2e/tests

--- e2e/tests/logintest.spec.js
+++ e2e/tests/logintest.spec.js
@@ -1,10 +1,11 @@
 const { test } = require('@playwright/test');
 const { LoginPage } = require('../pages/loginPage');
+
 test.describe('OrangeHRM Login Scenarios', () => {
-test('should successfully log in with valid credentials', async ({ page}) => {
-const loginPage = new LoginPage(page);
-await loginPage.goto();
-await loginPage.login('Admin', 'admin123');
-await loginPage.expectLoginSuccess();
-});
+  test('should successfully log in with valid credentials', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await loginPage.login('Admin', 'admin123');
+    await loginPage.expectLoginSuccess();
+  });
 });
